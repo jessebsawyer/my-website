cde7e1fa4a8a4151b04219111f95bba9c89d7a03
Jesse Sawyer <jessebsawyer@gmail.com>
2025-05-09 22:23:40 -0400

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import Layout from './components/Layout/Layout'
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -26,7 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Layout>{children}</Layout>
+        <Layout>
+          {children}
+        </Layout>
+        <Analytics />
       </body>
     </html>
   )
